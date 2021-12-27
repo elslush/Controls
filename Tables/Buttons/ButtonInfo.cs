b8a7fc6666900d0d5a35b3ecbdb7d042d76bff57
Matthew Ormson <matthew@ormson.me>
2021-12-27 00:26:42 -0800

    Layout

---
+++ Tables/Buttons/ButtonInfo.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+using static Controls.ColorPickers.ColorExtensions;
+
+namespace Controls.Buttons
+{
+    public readonly struct ButtonInfo
+    {
+        private static readonly Color defaultBackground = Color.FromArgb(94, 164, 228),
+            defaultText = Color.White;
+        private static readonly ButtonInfo defaultButtonInfo = new(null, null, null, null, null, null);
+
+        public static ButtonInfo Default => defaultButtonInfo;
+
+        public static Color DefaultBackground => defaultBackground;
+
+        public static Color DefaultText => defaultText;
+
+        public ButtonInfo(Color? backGround, Color? backgroundHover, Color? text, Color? textHover, Color? clicked, Color? clickedHover)
+        {
+            Color backgroundColor = backGround is null ? defaultBackground : (Color)backGround;
+            Color textColor = text is null ? defaultText : (Color)text;
+
+            Background = backgroundColor.ToHslStyle();
+            BackgroundHover = backgroundHover is null ? backgroundColor.ToDarkerHslStyle() : ((Color)backgroundHover).ToHslStyle();
+            Text = textColor.ToHslStyle();
+            TextHover = textHover is null ? textColor.ToDarkerHslStyle() : ((Color)textHover).ToHslStyle();
+            Clicked = clicked is null ? BackgroundHover : ((Color)clicked).ToHslStyle();
+            ClickedHover = clickedHover is null ? TextHover : ((Color)clickedHover).ToHslStyle();
+        }
+
+        public string Background { get; }
+
+        public string BackgroundHover { get; }
+
+        public string Text { get; }
+
+        public string TextHover { get; }
+
+        public string Clicked { get; }
+
+        public string ClickedHover { get; }
+    }
+}
---
+++ Tables/ColorPickers/ColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controls.ColorPickers
+{
+    public static class ColorExtensions
+    {
+        public static string ToHslStyle(this Color color) => $"hsl({color.GetHue()}, {color.GetSaturation() * 100}%, {color.GetBrightness() * 100}%)";
+
+        public static string ToDarkerHslStyle(this Color color) => $"hsl({color.GetHue()}, {color.GetSaturation() * 100}%, {color.GetBrightness() * 100 / 1.5}%)";
+        public static string ToRGBStyle(this Color color) => $"rgb({color.R}, {color.G}%, {color.B}%)";
+    }
+}
