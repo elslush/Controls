f2c0b5f7a675619d35ad1eb968e05353f6f68fd1
Matthew Ormson <matthew@ormson.me>
2022-01-11 14:04:28 -0500

    alerts, svgs

---
+++ Tables/Alerts/AlertPublisher.cs
@@ -0,0 +1,48 @@
+﻿using Controls.DynamicComponents;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Immutable;
+using System.Drawing;
+
+namespace Controls.Alerts
+{
+    public class AlertPublisher
+    {
+        private readonly LinkedList<ComponentMetadata> alerts = new();
+        private readonly uint maximumAlerts;
+
+        public AlertPublisher(uint maximumAlerts = 1)
+        {
+            this.maximumAlerts = maximumAlerts;
+        }
+
+        public event EventHandler? OnAlertsChanged;
+
+        public IEnumerable<ComponentMetadata> Alerts => alerts;
+
+        public void AddAlert(ComponentMetadata componentMetadata)
+        {
+            if (alerts.Count >= maximumAlerts)
+                alerts.RemoveFirst();
+            alerts.AddLast(componentMetadata);
+            OnAlertsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void RemoveAlert(ComponentMetadata componentMetadata)
+        {
+            alerts.Remove(componentMetadata);
+            OnAlertsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void CreateBasicAlert(string text, Color backgroundColor)
+        {
+            ComponentMetadata alert = new(typeof(BasicAlert));
+            alert.Parameters = new Dictionary<string, object>()
+            {
+                { "Text", text },
+                { "BackgroundColor", backgroundColor },
+                { "OnClose", EventCallback.Factory.Create(this, () => RemoveAlert(alert)) },
+            }.ToImmutableDictionary();
+            AddAlert(alert);
+        }
+    }
+}
---
+++ Tables/DynamicComponents/ComponentMetadata.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Immutable;
+
+namespace Controls.DynamicComponents
+{
+    public class ComponentMetadata
+    {
+        public ComponentMetadata(Type componentType) => ComponentType = componentType;
+
+        public Type ComponentType { get; }
+
+        public ImmutableDictionary<string, object> Parameters { get; set; } = new Dictionary<string, object>().ToImmutableDictionary();
+    }
+}
