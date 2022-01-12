7748f838e847ee8bd1f4cdf19458d1cf912db4df
Matthew Ormson <matthew@ormson.me>
2022-01-12 14:54:26 -0500

    tab, color

---
+++ Tables/Colors/CssColors/BorderBottomColor.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+
+namespace Controls.Colors.CssColors
+{
+    public readonly struct BorderBottomColor : ICssColor
+    {
+        private const string cssName = "border-bottom";
+
+        public BorderBottomColor(Color color) => Css = $"{cssName}:{color.ToRGBStyle()};";
+
+        public string Css { get; }
+    }
+}
